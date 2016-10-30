a4e8145a8b5830b58b0b4995938bef62dd23b15d
LoadingPleaseWait <michael@murphey.org>
2016-10-30 05:55:17 -0500

    Just read characters from server and use a flame sensor

--- SafetyRaptor-Edison.ino
+++ SafetyRaptor-Edison.ino
@@ -2,8 +2,8 @@
 /*
   Control servos off of a web server
 
-  A lot of this code was taken from examples 
- */
+  A lot of this code was taken from examples
+*/
 
 #include <Servo.h>
 #include <SPI.h>
@@ -13,17 +13,17 @@
 #define RIGHT_SERVO 10
 #define FLAME_SENSOR A0
 
-char ssid[] = "UHWireless"; //  your network SSID (name) 
+char ssid[] = "UHWireless"; //  your network SSID (name)
 
 int status = WL_IDLE_STATUS;
 
 // if you don't want to use DNS (and reduce your sketch size)
 // use the numeric IP instead of the name for the server:
-IPAddress server(172,25,75,108);  // numeric IP (no DNS)
+IPAddress server(172, 25, 75, 108); // numeric IP (no DNS)
 //char server[] = "www.google.com";    // name address (using DNS)
 
 // Initialize the Ethernet client library
-// with the IP address and port of the server 
+// with the IP address and port of the server
 // that you want to connect to (port 80 is default for HTTP):
 WiFiClient client;
 
@@ -32,40 +32,40 @@ Servo right_drive;
 
 void setup() {
   //Initialize serial and wait for port to open:
-  Serial.begin(9600); 
+  Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
-  
+
   // check for the presence of the shield:
   if (WiFi.status() == WL_NO_SHIELD) {
-    Serial.println("WiFi shield not present"); 
+    Serial.println("WiFi shield not present");
     // don't continue:
-    while(true);
-  } 
+    while (true);
+  }
 
   String fv = WiFi.firmwareVersion();
-  if( fv != "1.1.0" )
+  if ( fv != "1.1.0" )
     Serial.println("Please upgrade the firmware");
-  
+
   // attempt to connect to Wifi network:
-  while (status != WL_CONNECTED) { 
+  while (status != WL_CONNECTED) {
     Serial.print("Attempting to connect to SSID: ");
     Serial.println(ssid);
-    // Connect to open network   
+    // Connect to open network
     status = WiFi.begin(ssid);
-  
+
     // wait 10 seconds for connection:
     delay(10 * 1000);
-  } 
+  }
   Serial.println("Connected to wifi");
   printWifiStatus();
-  
+
   Serial.println("\nStarting connection to server...");
   // if you get a connection, report back via serial:
   if (client.connect(server, 2585)) {
     Serial.println("connected to server");
-    // Make a HTTP request:
+    // Make request:
     client.println("118");
   }
 
@@ -74,32 +74,32 @@ void setup() {
 }
 
 void loop() {
-  // if there are incoming bytes available 
+  // if there are incoming bytes available
   // from the server, read them and print them:
-  while (true) {
-    byte input = client.read();
-    Serial.println((int)input);
-    left_drive.write(input);
-    delay(15);
-    input = client.read();
-    Serial.println((int)input);
-    right_drive.write(input);
-    delay(500);
-
-    if(analogRead(FLAME_SENSOR) > 700) {
-      Serial.println("ROBOT ON FIRE! RUN AWAY!");
-    }
+  char input = client.read();
+  Serial.println(input);
+  if (input == 'L') {
+    // left turn
+    left_drive.write(-180);
+    right_drive.write(-180);
+  } else if (input == 'R') {
+    // right turn
+    left_drive.write(180);
+    right_drive.write(180);
+  } else if (input == 'F') {
+    // forward
+    left_drive.write(180);
+    right_drive.write(-180);
+  } else if (input == 'B') {
+    // backward
+    left_drive.write(-180);
+    right_drive.write(180);
   }
 
-  // if the server's disconnected, stop the client:
-  if (!client.connected()) {
-    Serial.println();
-    Serial.println("disconnecting from server.");
-    client.stop();
+  delay(500);
 
-    // do nothing forevermore:
-    while(true){
-    }
+  if (analogRead(FLAME_SENSOR) > 700) {
+    Serial.println("ROBOT ON FIRE! RUN AWAY!");
   }
 }
 
